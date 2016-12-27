3ed810eab9a26428c0af23275ea5092365c49171
Kentran2809 <tranmr@gmail.com>
2016-12-27 21:05:33 +1100

    Send message to only ppl in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -31,6 +31,16 @@ socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
+socket.on('updateUserList', function (users) {
+  var ol = jQuery('<ol></ol>')
+
+  users.forEach(function (user) {
+    ol.append(jQuery('<li></li>').text(user));
+  });
+
+  jQuery('#users').html(ol);
+});
+
 
 socket.on('newMessage', function (message) {
   var formattedTime = moment(message.createAt).format('h:mm a');
@@ -63,7 +73,6 @@ jQuery('#message-form').on('submit', function (e) {
   var messageTextBox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextBox.val()
   }, function () {
     messageTextBox.val('');
